89eb86f30a38bbe777848d8e21a9b5ad6cd0d8f6
aalexebr <alex.ebr@icloud.com>
2023-07-24 13:17:51 +0200

    ADD basic HEADER

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,37 @@ import {store} from '../store'
 export default{
     data(){
       return{
-        store
+        store,
+        navLinks:[
+            {
+                text:'home',
+                link:'#'
+            },
+            {
+                text:'about us',
+                link:'#'
+            },
+            {
+                text:'vehicles',
+                link:'#'
+            },
+            {
+                text:'dealers',
+                link:'#'
+            },
+            {
+                text:'packages',
+                link:'#'
+            },
+            {
+                text:'blog',
+                link:'#'
+            },
+            {
+                text:'contact',
+                link:'#'
+            },
+        ],
       }
     },
     components: {
@@ -14,7 +44,37 @@ export default{
 
 <template>
 <header>
-    HEADER
+    <div class="container">
+        <div class="row align-center">
+            <a href="#">
+                <div class="logo">
+                    <img src="../assets/img/logo-autocar11.png" alt="">
+                </div>
+            </a>
+            
+            <nav class="row align-center">
+                <ul>
+                    <li v-for="(link, index) in navLinks" :key="index">
+                        <a href="">
+                            {{ link.text }}
+                        </a>
+                    </li>
+                </ul>
+                <a href="#" class="button">
+                    my account
+                </a>
+                <a href="#" class="button">
+                    add car
+                </a>
+                <div class="hamburger">
+                    I
+                </div>
+                <a href="#" class="button">
+                    {{ store.contactNumber }}
+                </a>
+            </nav>
+        </div>
+    </div>
 </header>
 </template>
 
@@ -22,4 +82,24 @@ export default{
 @use '../assets/scss/main.scss' as *;
 @use '../assets/scss/debugger.scss' as *;
 @use '../assets/scss/utilities.scss' as *;
+
+header{
+    @include debug-large-two;
+}
+.logo{
+    width: 100px;
+}
+nav{
+    ul{
+        li{
+            display: inline-block;
+            padding: 5px;
+            margin: 0 5px;
+            text-transform: capitalize;
+        }
+    }
+    .button{
+        margin: 0 5px;
+    }
+}
 </style>
